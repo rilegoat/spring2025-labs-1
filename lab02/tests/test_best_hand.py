df5a366fc54bb75fa620e1f279c96a7bc1398237
Pulin Agrawal <pulinagrawal@gmail.com>
2025-01-19 12:05:11 -0500

    Implement best_hand function and update README with lab tasks; add unit tests for best_hand

---
+++ lab02/tests/test_best_hand.py
@@ -0,0 +1,20 @@
+'''
+Test the best_hand function.
+'''
+
+import itertools
+from random import shuffle
+from lab02.lab02 import best_hand
+
+def solution(hand):
+  pass
+
+def test_best_hand():
+  ranks = '23456789TJQKA'
+  suite = 'CDHS'
+  deck = [r+s for r in ranks for s in suite]
+  shuffle(deck)
+  for i, hand in enumerate(itertools.combinations(deck, 7)):
+    if i == 20:
+      break
+    assert best_hand(hand) == solution(hand)
