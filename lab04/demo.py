1281c26c47a462bc42b6d992f8bc4ac38ca304fa
Pulin Agrawal <pulinagrawal@gmail.com>
2025-02-04 11:36:49 -0500

    Fix import path in test_best_hand and update variable name in demo.py for clarity

--- lab04/demo.py
+++ lab04/demo.py
@@ -7,8 +7,8 @@ from lab04 import run_console_chat
 
 if __name__ ==  '__main__':
     #  run lab04.py to test your template
-    trader_template_file = 'lab04/demo_template.json'
-    run_console_chat(template_file=trader_template_file,
+    recruit_template_file = 'lab04/demo_template.json'
+    run_console_chat(template_file=recruit_template_file,
                      recruit_difficulty='not easy',
                      reward='a sword',
                      sign='Pulin',
