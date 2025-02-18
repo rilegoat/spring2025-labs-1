9a08503045b68c8a8c84836a166a03e7f1562bc8
Pulin Agrawal <pulinagrawal@gmail.com>
2025-02-18 11:54:16 -0500

    Fix type casting for difficulty check in roll_for function

--- lab05/lab05.py
+++ lab05/lab05.py
@@ -17,7 +17,7 @@ def roll_for(skill, dc, player):
     n_dice = 1
     sides = 20
     roll = sum([random.randint(1, sides) for _ in range(n_dice)])
-    if roll >= dc:
+    if roll >= int(dc):
         return f'{player} rolled {roll} for {skill} and succeeded!'
     else:
         return f'{player} rolled {roll} for {skill} and failed!'
